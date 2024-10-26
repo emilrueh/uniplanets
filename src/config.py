d62ef752235a88c98f2dcf0c68e0706c9a2fde08
emilrueh <emilrueh@googlemail.com>
2024-10-26 15:33:16 +0200

    Add beach planet terrain and clouds and change other values

--- src/config.py
+++ src/config.py
@@ -4,12 +4,9 @@ import random
 
 
 # display settings
-resolution, upscale = "1920x1080", 0.1
+# resolution, upscale = "1920x1080", 0.1
+resolution, upscale = "1000x1000", 0.1
 fps = 30
-
-if upscale > 0.1:
-    print("\nWARNING: resolution upscale has been set to at least 0.1 due to cloud shadows bug in current implementation!\n")
-    upscale = 0.1
 
 screen_width, screen_height = int(int(resolution.split("x")[0]) * upscale), int(int(resolution.split("x")[-1]) * upscale)
 print(f"{screen_width}x{screen_height} {fps}fps")
@@ -21,17 +18,14 @@ background_color = pick_color("black")
 
 angle_of_light = set_time_of_day("day")
 
-base_lighting = Lighting(angle=angle_of_light, speed=0.01, intensity=1.0)
+base_lighting = Lighting(angle=angle_of_light, speed=0.03, intensity=1.0)
 base_planet_rotation = Rotation(direction="left", speed=0.01, axis="y", angle=0.0)
 base_radius = int(((screen_width + screen_height) // 4) * 0.6)
 base_position = Vector(x=screen_width // 2, y=screen_height // 2)
 
 terrains = {
     "earth": [
-        Terrain(name="deep_water", color=RGB(50, 112, 211), threshold=0.55),
-        Terrain(name="shallow_water", color=RGB(100, 152, 234), threshold=0.6),
-        Terrain(name="coast", color=RGB(238, 232, 169), threshold=0.62),
-        Terrain(name="land", color=RGB(140, 185, 122), threshold=0.7),
+        Terrain(name="deep_water", color=RGB(50, 112, 211), threshold=0.5),
         Terrain(name="forest", color=RGB(84, 161, 109), threshold=float("inf")),
     ],
     "moon": [
@@ -52,39 +46,55 @@ terrains = {
         Terrain(name="lava", color=RGB(127, 32, 21), threshold=0.56),
         Terrain(name="magma", color=RGB(244, 165, 61), threshold=float("inf")),
     ],
+    "atollo": [
+        Terrain(name="deep_water", color=RGB(50, 112, 211), threshold=0.45),
+        Terrain(name="shallow_water", color=RGB(100, 152, 234), threshold=0.55),
+        Terrain(name="coast", color=RGB(238, 232, 169), threshold=0.6),
+        Terrain(name="land", color=RGB(140, 185, 122), threshold=0.65),
+        Terrain(name="forest", color=RGB(84, 161, 109), threshold=float("inf")),
+    ],
 }
 
 clouds = {
     "earth": Clouds(
-        height=1.1,
+        height=1.03,
         color=RGB(255, 255, 255),
-        alpha=220,
-        threshold=0.55,
-        lod=LevelOfDetail(4, 2),
-        rotation=Rotation(direction="left", speed=0.01, axis=["y", "z"]),
+        alpha=240,
+        threshold=0.6,
+        lod=LevelOfDetail(7, 2),
+        rotation=Rotation(direction="left", speed=0.001, axis=["y", "z"]),
     ),
     "mars": Clouds(
-        height=1.1,
-        color=RGB(255, 220, 160),
+        height=1.04,
+        color=RGB(255, 210, 160),
         alpha=200,
         threshold=0.6,
         lod=LevelOfDetail(4),
         rotation=Rotation(direction="right", speed=0.005, axis=["y", "z"]),
     ),
     "eve": Clouds(
-        height=1.15,
+        height=1.09,
         color=RGB(150, 150, 255),
-        alpha=200,
+        alpha=220,
         threshold=0.59,
-        lod=LevelOfDetail(),
-        rotation=Rotation(direction="left", speed=0.01, axis=["x", "z"]),
+        lod=LevelOfDetail(5),
+        rotation=Rotation(direction="left", speed=0.001, axis=["x", "z"]),
     ),
     "doom": Clouds(
-        height=1.1,
+        height=1.06,
         color=RGB(50, 50, 50),
         alpha=255,
         threshold=0.45,
         lod=LevelOfDetail(3),
         rotation=Rotation(direction="right", speed=0.01, axis="y"),
+        shadow_alpha=0.2,
+    ),
+    "atollo": Clouds(
+        height=1.05,
+        color=RGB(255, 255, 255),
+        alpha=200,
+        threshold=0.55,
+        lod=LevelOfDetail(4.5, 0.5),
+        rotation=Rotation(direction="left", speed=0.003, axis=["y", "z"]),
     ),
 }
